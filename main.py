3f376022a3d52278d63ff06773bf84ddd45b7ed4
Rohit <rohitkale523@gmail.com>
2024-03-19 19:22:23 +0530

    added pandas import and result is column names

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from typing import Union
-
+import pandas as pd
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 
 @app.post("/upload/csv/")
